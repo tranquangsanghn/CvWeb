02893ac32a34f182156e0e8cadd6cbe478618ae4
tranquangsanghn <tranquangsanghn@gmail.com>
2020-07-28 01:51:24 +0700

    Hibernated ok

--- CvWeb/src/main/java/com/Model/User.java
+++ CvWeb/src/main/java/com/Model/User.java
@@ -1,19 +1,45 @@
 package com.Model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "user")
 public class User {
+
 	private String userName;
 	private String password;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // this is where problem exists
+	@Column(name = "user_id", updatable = false, nullable = false)
+	private Long user_id;
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+
+	public Long getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(Long user_id) {
+		this.user_id = user_id;
+	}
+
 }
--- CvWeb/src/main/java/com/controller/Login.java
+++ CvWeb/src/main/java/com/controller/Login.java
@@ -1,6 +1,13 @@
 package com.controller;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.exception.JDBCConnectionException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,9 +20,27 @@ import com.Model.User;
 public class Login {
 
 	@GetMapping
+	@Transactional
 	public String setHome() {
+		
+		String sql = "FROM user";
+		try {
+			Session session = sessionFactory.getCurrentSession();
+			List<User> list = session.createQuery(sql).getResultList();
+			for (User user : list) {
+				System.out.println(user.getUserName() + user.getPassword());
+			}
+		} catch (JDBCConnectionException e) {
+			// TODO: handle exception
+		}
+		
+		
+		
 		return "Login";
 	}
+	
+	@Autowired
+	SessionFactory sessionFactory;
 
 	@PostMapping
 	public String checkUser(@ModelAttribute User user) {
@@ -25,4 +50,5 @@ public class Login {
 			return "Login";
 		}
 	}
+
 }
